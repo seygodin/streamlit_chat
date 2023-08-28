e3da9a90761836daff7db76edbc703ba7f45b305
seygodin <111480823+seygodin@users.noreply.github.com>
2023-08-28 11:35:22 +0900

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -6,11 +6,31 @@ import streamlit as st
 your_api_key = st.text_input(label="YOUR OPENAI API KEY")
 
 if your_api_key:
-    chatbot = ChatOpenAI(openai_api_key=your_api_key, max_tokens=1000)
+    query_develop_model = ChatOpenAI(openai_api_key=your_api_key)
 
-query = st.text_input(label="User Query")
+    query_check_model = ChatOpenAI(openai_api_key=your_api_key)
+    
+    query_model = ChatOpenAI(openai_api_key=your_api_key)
+    
 
-if query:
-    response = chatbot.predict(query)
+    #query from user
+    query = st.text_input(label="User Query")
+    run_buttton = st.button("Run AI")
+    if run_buttton:
+        st.text_area("User Query",  query, height=100)
 
-    st.text_area("Answer", response, height=600)
+
+        developed_query = query_develop_model.predict(f"My query is {query}. Make it more specific and detailed query for better quenstioning")
+        if developed_query:
+            st.text_area("1st developed query", developed_query, height=150)
+
+        run = False
+        checked_query = query_check_model.predict(f"My query is {developed_query}. Make it more like query for questioniung.")
+        if checked_query:
+            st.text_area("2nd refined query", checked_query, height=150)
+            run = True
+            
+        if run:
+            response = query_model.predict(checked_query)
+
+            st.text_area("Answer", response, height=600)
